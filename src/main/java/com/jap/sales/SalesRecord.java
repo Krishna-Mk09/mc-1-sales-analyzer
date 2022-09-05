71b6ec8b39e1e93cca11a9477edc0f5f825a08c6
M V K <krishna.mugala9@gmail.com>
2022-09-05 20:01:55 +0530

    added the source code and formatted the code

--- src/main/java/com/jap/sales/SalesRecord.java
+++ src/main/java/com/jap/sales/SalesRecord.java
@@ -1,31 +1,88 @@
 package com.jap.sales;
 
-import java.util.Date;
-
 public class SalesRecord {
     private String date;
-    private int customer_id;
-    private int product_category;
-    private String payment_method;
+    private int customerId;
+    private int productCategory;
+    private String paymentMethod;
     private double amount;
-    private double time_on_site;
-    private int clicks_in_site;
-
+    private double timeOnSite;
+    private int clicksInSite;
 
     // Generate Constructors
-
-    public SalesRecord(String date, int customer_id, int product_category, String payment_method, double amount, double time_on_site, int clicks_in_site) {
+    public SalesRecord(String date, int customerId, int productCategory, String paymentMethod, double amount, double timeOnSite, int clicksInSite) {
         this.date = date;
-        this.customer_id = customer_id;
-        this.product_category = product_category;
-        this.payment_method = payment_method;
+        this.customerId = customerId;
+        this.productCategory = productCategory;
+        this.paymentMethod = paymentMethod;
         this.amount = amount;
-        this.time_on_site = time_on_site;
-        this.clicks_in_site = clicks_in_site;
+        this.timeOnSite = timeOnSite;
+        this.clicksInSite = clicksInSite;
+    }
+
+    public SalesRecord() {
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public int getProductCategory() {
+        return productCategory;
+    }
+
+    public void setProductCategory(int productCategory) {
+        this.productCategory = productCategory;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public double getTimeOnSite() {
+        return timeOnSite;
+    }
+
+    public void setTimeOnSite(double timeOnSite) {
+        this.timeOnSite = timeOnSite;
+    }
+
+    public int getClicksInSite() {
+        return clicksInSite;
+    }
+
+    public void setClicksInSite(int clicksInSite) {
+        this.clicksInSite = clicksInSite;
     }
 
     // Generate Getters and Setters
-
     // Generate the toString method
-
+    @Override
+    public String toString() {
+        return "SalesRecord{" + "date='" + date + '\'' + ", customer_id=" + customerId + ", product_category=" + productCategory + ", payment_method='" + paymentMethod + '\'' + ", amount=" + amount + ", time_on_site=" + timeOnSite + ", clicks_in_site=" + clicksInSite + '}';
+    }
 }
